20661765f4f0bc552159e9cc5b671246290fd59d
habbiadhi <habbiadhi@gmail.com>
2022-11-05 15:37:49 +0700

    add java faker with mapstruct and init for swagger

--- src/main/java/com/habbiadhi/mapstruct/config/SwaggerConfig.java
+++ src/main/java/com/habbiadhi/mapstruct/config/SwaggerConfig.java
@@ -1,33 +1,35 @@
 package com.habbiadhi.mapstruct.config;
 
-//@Configuration
-//@EnableSwagger2
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.habbiadhi.mapstruct"))
+                .paths(PathSelectors.ant("/path/**"))
+                .build();
+    }
 
-//    @Bean
-//    public Docket postsApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
-//                .apiInfo(apiInfo()).select().paths(postPaths()).build();
-//    }
-//
-//    private Predicate<String> postPaths() {
-//        return or(regex("/path.*"));
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("JavaInUse API")
-//                .description("JavaInUse API reference for developers")
-//                .termsOfServiceUrl("http://javainuse.com")
-//                .contact("javainuse@gmail.com").license("JavaInUse License")
-//                .licenseUrl("javainuse@gmail.com").version("1.0").build();
-//    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Spring boot setup for API")
+                .description("JavaInUse API reference for developers")
+                .termsOfServiceUrl("http://javainuse.com")
+                .license("JavaInUse License")
+                .licenseUrl("javainuse@gmail.com").version("1.0").build();
+    }
 }
