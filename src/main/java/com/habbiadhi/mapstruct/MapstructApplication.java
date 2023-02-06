a6c0dc31b485d800f3e084a1868a821ff828b3ba
habbiadhi <habbiadhi@gmail.com>
2023-02-07 02:17:42 +0700

    implement tee filter

--- src/main/java/com/habbiadhi/mapstruct/MapstructApplication.java
+++ src/main/java/com/habbiadhi/mapstruct/MapstructApplication.java
@@ -1,7 +1,10 @@
 package com.habbiadhi.mapstruct;
 
+import ch.qos.logback.access.servlet.TeeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MapstructApplication {
@@ -10,18 +13,10 @@ public class MapstructApplication {
 		SpringApplication.run(MapstructApplication.class, args);
 	}
 
-//	@Bean
-//	public FilterRegistrationBean requestResponseFilter() {
-//
-//		final FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
-//		List<String> urlPattern = new ArrayList<>();
-//		urlPattern.add("/path");
-//
-//		TeeFilter filter = new TeeFilter();
-//		filterRegBean.setFilter(filter);
-//		filterRegBean.setUrlPatterns(urlPattern);
-//		filterRegBean.setName("Request Response Filter");
-//		filterRegBean.setAsyncSupported(Boolean.TRUE);
-//		return filterRegBean;
-//	}
+	@Bean
+	public FilterRegistrationBean requestResponseFilter() {
+		final FilterRegistrationBean filterRegBean = new FilterRegistrationBean(new TeeFilter());
+		filterRegBean.setName();
+		return filterRegBean;
+	}
 }
