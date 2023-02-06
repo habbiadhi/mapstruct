b7ecb3eb26a04605d9dfaec34f77d24170a7162f
habbiadhi <habbiadhi@gmail.com>
2023-02-06 23:21:16 +0700

    implement mapstruct 3.0

--- README.md
+++ README.md
@@ -6,3 +6,42 @@ Library
 - Java Faker  : To generate Data
 - Mapstruct   : For domain mapping
 - Swagger     : For Api Documentation
+
+Link to Open Swagger Documentation : http://localhost:8080/v2/api-docs
+
+### Swagger 2.0
+
+url ui : http://localhost:8080/swagger-ui.html#/
+
+		<dependency>
+			<groupId>io.springfox</groupId>
+			<artifactId>springfox-swagger2</artifactId>
+			<version>2.9.2</version>
+		</dependency>
+
+		<dependency>
+			<groupId>io.springfox</groupId>
+			<artifactId>springfox-swagger-ui</artifactId>
+			<version>2.9.2</version>
+		</dependency>
+
+### Swagger 3.0
+
+url ui : http://localhost:8080/swagger-ui/
+
+    <dependency>
+        <groupId>io.springfox</groupId>
+        <artifactId>springfox-boot-starter</artifactId>
+        <version>3.0.0</version>
+    </dependency>
+
+jika mendapatkan error  :heavy_multiplication_x:
+    
+    javax.servlet.ServletException: Could not resolve view with name 'forward:/swagger-ui/index.html' in servlet with name 'dispatcherServlet'
+
+perlu tambahkan bean di swagger config file
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();  
+    }
--- src/main/java/com/habbiadhi/mapstruct/config/SwaggerConfig.java
+++ src/main/java/com/habbiadhi/mapstruct/config/SwaggerConfig.java
@@ -1,26 +1,40 @@
 package com.habbiadhi.mapstruct.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-//@EnableWebMvc
-//@EnableSwagger2
+@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.habbiadhi.mapstruct"))
+                .paths(PathSelectors.ant("/path/**"))
+                .build();
+    }
+
 //    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.habbiadhi.mapstruct"))
-//                .paths(PathSelectors.ant("/path/**"))
-//                .build();
+//    public InternalResourceViewResolver defaultViewResolver() {
+//        return new InternalResourceViewResolver();
 //    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("Spring boot setup for API")
-//                .description("JavaInUse API reference for developers")
-//                .termsOfServiceUrl("http://javainuse.com")
-//                .license("JavaInUse License")
-//                .licenseUrl("javainuse@gmail.com").version("1.0").build();
-//    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Spring boot setup for API")
+                .description("JavaInUse API reference for developers")
+                .termsOfServiceUrl("http://javainuse.com")
+                .license("JavaInUse License")
+                .licenseUrl("javainuse@gmail.com").version("1.0").build();
+    }
 }
